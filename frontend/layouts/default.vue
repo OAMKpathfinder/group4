eb8e935f4e2b434b2c5dda57e47a22eb104c6a02
Arpad Gabor <arpig.razzey@gmail.com>
2019-11-05 00:57:36 +0200

    created frontend project with Nuxt

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto">
+    <div>
+      <h1 class="text-3xl">frontend</h1>
+      <h2 class="subtitle">Pathfinder project Web App made with Nuxt.js</h2>
+      <div class="links--fix">
+        <nuxt-link class="font-bold no-underline" to="about">About</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
